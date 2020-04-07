28d8e1526ac193f5837dc6aabaf272b101e14d75
lekstrom <39048645+lekstrom@users.noreply.github.com>
2020-04-07 20:46:28 +0200

    Update Person.cs
    Här är några förslag till ändringar av Person klassen. Jag lade till properties och uppdaterade konstruktorn. / Lowe

--- ALGODS-Projekt/ALGODS-Projekt/Person.cs
+++ ALGODS-Projekt/ALGODS-Projekt/Person.cs
@@ -10,7 +10,53 @@ namespace ALGODS_Projekt
     {
         public Person(int start_floor, int end_floor)
         {
-            int time = 0;
+            _Start_floor = start_floor;
+            _End_floor = end_floor;
+            _Time = 0;
         }
+        
+        
+        // Properties. Looked up how to use properties in Essential C# 6.0 by Mark Michaelis and Eric Lippert, p. 237 - 240.
+        
+        public int Start_floor
+        {
+            get 
+            {
+                return _Start_floor;
+            }
+            set
+            {
+                _Start_floor = value;
+            }
+        }
+        private int _Start_floor;
+        
+        public int End_floor
+        {
+            get
+            {
+                return _End_floor;
+            }
+            set
+            {
+                _End_floor = value;
+            }
+        }
+        private int _End_floor;
+        
+        public int Time 
+        {
+            get
+            {
+                return _Time;
+            }
+            set 
+            {
+                _Time = value;
+            }
+        }
+        private int _Time;
+        
+        
     }
 }

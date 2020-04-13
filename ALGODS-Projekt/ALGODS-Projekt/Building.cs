53c57c139b830c571e38ac8c2a221e4eced0bd98
Lowe Ekström <loweekstrom@hotmail.com>
2020-04-13 11:12:10 +0200

    Kanske en klass som ser ut någonting åt det här hållet?

--- ALGODS-Projekt/ALGODS-Projekt/Building.cs
+++ ALGODS-Projekt/ALGODS-Projekt/Building.cs
@@ -9,8 +9,27 @@ namespace ALGODS_Projekt
     class Building
     {
 
+        public Building()
+        {
+            elevator = new Elevator();
+        }
 
-        
+        // Byggnaden får ett elevator-objekt vid instansiering
+        Elevator elevator;
+
+        // Våningar
+        List<Person> Floor1;
+        List<Person> Floor2;
+        List<Person> Floor3;
+        List<Person> Floor4;
+        List<Person> Floor5;
+        List<Person> Floor6;
+        List<Person> Floor7;
+        List<Person> Floor8;
+        List<Person> Floor9;
+        List<Person> Floor10;
+
+
 
     }
 }

9cef303ad6963a9faa0933639017f403ce01e311
Lowe Ekström <loweekstrom@hotmail.com>
2020-04-14 15:41:53 +0200

    Uppdatering av Building och Floor-klasserna

--- ALGODS-Projekt/ALGODS-Projekt/Building.cs
+++ ALGODS-Projekt/ALGODS-Projekt/Building.cs
@@ -17,17 +17,10 @@ namespace ALGODS_Projekt
         // Byggnaden får ett elevator-objekt vid instansiering
         Elevator elevator;
 
-        // Våningar
-        List<Person> Floor1;
-        List<Person> Floor2;
-        List<Person> Floor3;
-        List<Person> Floor4;
-        List<Person> Floor5;
-        List<Person> Floor6;
-        List<Person> Floor7;
-        List<Person> Floor8;
-        List<Person> Floor9;
-        List<Person> Floor10;
+        // Våningar - En lista med Floor-objekt?
+
+        List<Floor> floors;
+        
 
 
 
